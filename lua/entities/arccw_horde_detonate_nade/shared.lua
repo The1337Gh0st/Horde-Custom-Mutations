406955af1e13d263de2d593fa0fbc4a052187726
The1337Gh0st <cwarrior1414@gmail.com>
2022-09-24 23:22:02 -0400

    Added Puncture, Detonate buff
    Buffed Detonate grenade damage to 50
    
    Added Puncture, causes character to be tinted purple and damage ignores resistances

--- lua/entities/arccw_horde_detonate_nade/shared.lua
+++ lua/entities/arccw_horde_detonate_nade/shared.lua
@@ -79,7 +79,7 @@ function ENT:Detonate()
         if self.Owner:IsValid() then
             attacker = self.Owner
         end
-        util.BlastDamage(self, attacker, self:GetPos(), 200, 25)
+        util.BlastDamage(self, attacker, self:GetPos(), 200, 50)
 
         self:Remove()
 
